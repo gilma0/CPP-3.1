daf5fa39ec958895b4c2c5bae756917510efe932
gilma0 <44779445+gilma0@users.noreply.github.com>
2019-04-04 15:36:09 +0300

    Add files via upload

---
+++ PhysicalNumber.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include "Unit.h"
+#include "PhysicalNumber.h"
+
+using namespace std;
+
+namespace ariel{
+
+	double value;
+	Unit type;
+
+	PhysicalNumber::PhysicalNumber(double a, Unit type){
+		this->value = a;
+		this->type = type;
+	}
+	PhysicalNumber::PhysicalNumber(){
+		this->value = 0;
+		this->value = 500;
+	}
+	PhysicalNumber PhysicalNumber::operator+(){
+		PhysicalNumber temp;
+		temp.value = this->value;
+		temp.type = this->type;
+		return temp;
+	}
+	PhysicalNumber* PhysicalNumber::operator-(){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	PhysicalNumber* PhysicalNumber::operator-(PhysicalNumber a){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	PhysicalNumber* PhysicalNumber::operator-=(PhysicalNumber a){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	PhysicalNumber* PhysicalNumber::operator+=(PhysicalNumber a){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	PhysicalNumber* PhysicalNumber::operator+(PhysicalNumber a){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	bool PhysicalNumber::operator==(PhysicalNumber a){
+		return true;
+	}
+	PhysicalNumber* PhysicalNumber::operator=(PhysicalNumber a){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	bool PhysicalNumber::operator<=(PhysicalNumber a){
+		return true;
+	}
+	bool PhysicalNumber::operator>=(PhysicalNumber a){
+		return true;
+	}
+	bool PhysicalNumber::operator<(PhysicalNumber a){
+		return true;
+	}
+	bool PhysicalNumber::operator>(PhysicalNumber a){
+		return true;
+	}
+	PhysicalNumber* PhysicalNumber::operator++(){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	PhysicalNumber* PhysicalNumber::operator--(){
+		PhysicalNumber* temp = new PhysicalNumber(5, Unit::KM);
+		return temp;
+	}
+	std::ostream& operator<<(std::ostream& os, const PhysicalNumber& a){
+		os<<a.value<<endl<<a.type;
+		return os;
+	}
+	std::istream& operator>>(std::istream& is, PhysicalNumber& a){
+		is>>a.value;
+		return is;
+	}
+}
+
+/*
+
+int main(){
+	ariel::PhysicalNumber a;
+	a.value = 5;
+	a.type = ariel::Unit::HOUR;
+	cout<<a;
+	return 0;
+}
+
+*/
