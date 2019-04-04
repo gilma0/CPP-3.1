ea725bc0b7524bb0ee9bb3ae244ecb48a6bfb823
gilma0 <44779445+gilma0@users.noreply.github.com>
2019-04-04 16:19:41 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -4,28 +4,25 @@
 using namespace std;
 namespace ariel{
 	class PhysicalNumber{
-
-
-
 	public:
 		double value;
 		Unit type;
 		PhysicalNumber(double a, Unit);
 		PhysicalNumber();
-		PhysicalNumber* operator+(PhysicalNumber);
-		PhysicalNumber operator+();
-		PhysicalNumber* operator-(PhysicalNumber);
-		PhysicalNumber* operator-();
-		PhysicalNumber* operator-=(PhysicalNumber);
-		PhysicalNumber* operator+=(PhysicalNumber);
+		PhysicalNumber& operator+(PhysicalNumber);
+		PhysicalNumber& operator+();
+		PhysicalNumber& operator-(PhysicalNumber);
+		PhysicalNumber& operator-();
+		PhysicalNumber& operator-=(PhysicalNumber);
+		PhysicalNumber& operator+=(PhysicalNumber);
 		bool operator==(PhysicalNumber);
-		PhysicalNumber* operator=(PhysicalNumber);
+		PhysicalNumber& operator=(PhysicalNumber);
 		bool operator<=(PhysicalNumber);
 		bool operator>=(PhysicalNumber);
 		bool operator<(PhysicalNumber);
 		bool operator>(PhysicalNumber);
-		PhysicalNumber* operator++();
-		PhysicalNumber* operator--();
+		PhysicalNumber& operator++();
+		PhysicalNumber& operator--();
 	};
 	std::ostream& operator<<(std::ostream& os, const PhysicalNumber& a);
 	std::istream& operator>>(std::istream& is, PhysicalNumber& a);
